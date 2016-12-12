5bdff028e33f91e168043ac8b280a56e06eb2432
unknown <nbalboni2@gmail.com>
2016-12-11 19:25:28 -0500

    Made virtualenv relocatable

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\Nick\Code\school\tmp\secure-instant-messenger\venv"
+set "VIRTUAL_ENV=C:\Users\Nick\Code\school\secure-instant-messenger\.venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
@@ -9,7 +9,7 @@ if defined _OLD_VIRTUAL_PROMPT (
     )
     set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
 )
-set "PROMPT=(venv) %PROMPT%"
+set "PROMPT=(.venv) %PROMPT%"
 
 REM Don't use () to avoid problems with them in %PATH%
 if defined _OLD_VIRTUAL_PYTHONHOME goto ENDIFVHOME
